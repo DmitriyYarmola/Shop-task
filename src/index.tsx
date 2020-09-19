27e5652a7f23d64607835ed6b9745265054da8c6
Dmitriy <dmitriy.yarmola@gmail.com>
2020-09-19 04:04:02 +0300

    change on hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { App } from './App'
 
-ReactDOM.render(
-	<BrowserRouter>
+eactDOM.render(
+	<HashRouter>
 		<App />
-	</BrowserRouter>,
+	</HashRouter>,
 	document.querySelector('#app')
 )
